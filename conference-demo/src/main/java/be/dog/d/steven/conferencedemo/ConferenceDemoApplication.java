32a6350f6d815112a41167327478b8f019d28c5a
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-09-02 17:39:06 +0200

    :rocket: Initial commit

---
+++ conference-demo/src/main/java/be/dog/d/steven/conferencedemo/ConferenceDemoApplication.java
@@ -0,0 +1,13 @@
+package be.dog.d.steven.conferencedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceDemoApplication.class, args);
+    }
+
+}
