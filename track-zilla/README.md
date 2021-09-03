eebc6a46724a09bb467bde6e3af84c2d7769c930
Steven D'Hondt <steven.dhondt@esaturnus.com>
2021-09-03 09:42:36 +0200

    :rocket: Initial commit

---
+++ track-zilla/README.md
@@ -0,0 +1,15 @@
+# Spring Boot Fundamentals
+
+- Features of Spring Boot
+  - Spring Boot starters
+  - Auto configuration
+- Bootstrapping an application
+  - Spring Initializr
+- Data access
+- Spring MVC
+- REST API development
+- GraphQL servers
+- Spring Boot Actuator
+- Testing
+
+
---
+++ track-zilla/src/main/java/be/dog/d/steven/trackzilla/TrackZillaApplication.java
@@ -0,0 +1,13 @@
+package be.dog.d.steven.trackzilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackZillaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackZillaApplication.class, args);
+    }
+
+}
