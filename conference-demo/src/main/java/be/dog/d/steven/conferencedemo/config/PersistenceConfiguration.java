75debbcd176e05211fa60fe15d5d49fc16e731a5
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-09-04 22:39:58 +0200

    :sparkles: Datasource using Java-config

---
+++ conference-demo/src/main/java/be/dog/d/steven/conferencedemo/config/PersistenceConfiguration.java
@@ -0,0 +1,31 @@
+package be.dog.d.steven.conferencedemo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersistenceConfiguration {
+
+    @Value("${DB_URL}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+    
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url(url);
+        builder.username(username);
+        builder.password(password);
+        System.out.println("Custom datasource bean has been initialized and set.");
+        return builder.build();
+    }
+}
