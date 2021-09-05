8feedbc5c5031f57a51779d7e9edd1442d81da6c
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-09-05 21:54:02 +0200

    :sparkles: Servlet Container & Deployment

--- conference-demo/src/main/java/be/dog/d/steven/conferencedemo/config/PersistenceConfiguration.java
+++ conference-demo/src/main/java/be/dog/d/steven/conferencedemo/config/PersistenceConfiguration.java
@@ -1,11 +1,7 @@
 package be.dog.d.steven.conferencedemo.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
@@ -19,13 +15,13 @@ public class PersistenceConfiguration {
     @Value("${spring.datasource.password}")
     private String password;
     
-    @Bean
-    public DataSource dataSource() {
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url(url);
-        builder.username(username);
-        builder.password(password);
-        System.out.println("Custom datasource bean has been initialized and set.");
-        return builder.build();
-    }
+//    @Bean
+//    public DataSource dataSource() {
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url(url);
+//        builder.username(username);
+//        builder.password(password);
+//        System.out.println("Custom datasource bean has been initialized and set.");
+//        return builder.build();
+//    }
 }
