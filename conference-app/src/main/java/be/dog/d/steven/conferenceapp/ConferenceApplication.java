38bd74d1bf542984d8fc0a5f9e67fe135b8f15c9
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-09-19 23:24:15 +0200

    :rocket: Initial commit

--- conference-app/src/main/java/be/dog/d/steven/conferenceapp/ConferenceApplication.java
+++ conference-app/src/main/java/be/dog/d/steven/conferenceapp/ConferenceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConferenceAppApplication {
+public class ConferenceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConferenceAppApplication.class, args);
+        SpringApplication.run(ConferenceApplication.class, args);
     }
 
 }
