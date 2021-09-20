1e28de3fc5f01eec170c22ced211ab2d278ceaee
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-09-20 06:15:41 +0200

    :sparkles: i18n

--- conference-app/src/main/java/be/dog/d/steven/conferenceapp/config/ConferenceConfiguration.java
+++ conference-app/src/main/java/be/dog/d/steven/conferenceapp/config/ConferenceConfiguration.java
@@ -1,11 +1,19 @@
 package be.dog.d.steven.conferenceapp.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class ConferenceConfiguration implements WebMvcConfigurer {
@@ -29,5 +37,33 @@ public class ConferenceConfiguration implements WebMvcConfigurer {
         registry
                 .addResourceHandler("/styles/**")
                 .addResourceLocations("/WEB-INF/styles/");
+    }
+    
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+    
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry
+                .addInterceptor(localeChangeInterceptor());
+    }
+    
+    @Bean
+    public MessageSource messageSource() {
+        var messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:i18n/messages");
+        messageSource.setDefaultEncoding("ISO-8859-1");
+        return messageSource;
     }
 }
--- conference-app/src/main/webapp/WEB-INF/jsp/registration.jsp
+++ conference-app/src/main/webapp/WEB-INF/jsp/registration.jsp
@@ -7,6 +7,7 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 <html>
 <head>
     <title>Registration</title>
@@ -18,7 +19,7 @@
     <table>
         <tr>
             <td>
-                Name:
+                <spring:message code="name"/>:
             </td>
             <td>
                 <form:input path="name"/>
@@ -26,7 +27,9 @@
         </tr>
         <tr>
             <td colspan="2">
-                <input type="submit" value="Add Registration">
+                <button type="submit">
+                    <spring:message code="save.changes"/>
+                </button>
             </td>
         </tr>
     </table>
