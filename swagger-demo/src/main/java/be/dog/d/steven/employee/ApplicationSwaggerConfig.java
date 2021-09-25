5b78753d532bd999b7892cc4e3bccc34d2865389
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-09-26 01:03:02 +0200

    :sparkles: ApiInfo

--- swagger-demo/src/main/java/be/dog/d/steven/employee/ApplicationSwaggerConfig.java
+++ swagger-demo/src/main/java/be/dog/d/steven/employee/ApplicationSwaggerConfig.java
@@ -3,8 +3,11 @@ package be.dog.d.steven.employee;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -21,6 +24,17 @@ public class ApplicationSwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
+    
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Employee API")
+                .version("1.0")
+                .description("API for managing employees.")
+                .contact(new Contact("Steven D'Hondt", "https://github.com/H3AR7B3A7", "steven.d.hondt.sdh@gmail.com"))
+                .license("Apache License Version 2.0")
                 .build();
     }
 }
