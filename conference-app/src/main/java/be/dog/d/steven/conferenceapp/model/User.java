8247ad57460d910ef23d6cd0cb7107514d797ddc
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-09-25 17:11:39 +0200

    :sparkles: RestController and js front-end

---
+++ conference-app/src/main/java/be/dog/d/steven/conferenceapp/model/User.java
@@ -0,0 +1,31 @@
+package be.dog.d.steven.conferenceapp.model;
+
+public class User {
+    private String firstname;
+    private String lastname;
+    private Integer age;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
