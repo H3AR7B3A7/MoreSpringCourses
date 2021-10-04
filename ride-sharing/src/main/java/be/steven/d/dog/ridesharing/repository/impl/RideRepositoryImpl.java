18ed1adf426a860fc00afecf862e03b0ef8a1a36
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-10-04 03:02:37 +0200

    :sparkles: Dependencies & wiring

--- ride-sharing/src/main/java/be/steven/d/dog/ridesharing/repository/impl/RideRepositoryImpl.java
+++ ride-sharing/src/main/java/be/steven/d/dog/ridesharing/repository/impl/RideRepositoryImpl.java
@@ -2,6 +2,8 @@ package be.steven.d.dog.ridesharing.repository.impl;
 
 import be.steven.d.dog.ridesharing.model.Ride;
 import be.steven.d.dog.ridesharing.repository.RideRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,6 +11,10 @@ import java.util.List;
 
 @Repository("rideRepository")
 public class RideRepositoryImpl implements RideRepository {
+    
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    
     @Override
     public List<Ride> getRides() {
         Ride ride = new Ride();
