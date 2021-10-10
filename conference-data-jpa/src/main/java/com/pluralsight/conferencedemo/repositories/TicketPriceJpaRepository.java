6094eb27d4b6a82c2075dfda196132be0edc6702
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-10-10 14:38:02 +0200

    :sparkles: Implementing JpaRepository

---
+++ conference-data-jpa/src/main/java/com/pluralsight/conferencedemo/repositories/TicketPriceJpaRepository.java
@@ -0,0 +1,6 @@
+package com.pluralsight.conferencedemo.repositories;
+
+import com.pluralsight.conferencedemo.models.TicketPrice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TicketPriceJpaRepository extends JpaRepository<TicketPrice, Long> { }
