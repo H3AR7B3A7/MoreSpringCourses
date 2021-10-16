9dbc22d62466eaf342c6ab7de8eb0cd1c5b600fd
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-10-16 13:04:28 +0200

    :sparkles: And / Or example

--- conference-data-jpa/src/main/java/com/pluralsight/conferencedemo/repositories/SpeakerJpaRepository.java
+++ conference-data-jpa/src/main/java/com/pluralsight/conferencedemo/repositories/SpeakerJpaRepository.java
@@ -3,4 +3,9 @@ package com.pluralsight.conferencedemo.repositories;
 import com.pluralsight.conferencedemo.models.Speaker;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SpeakerJpaRepository extends JpaRepository<Speaker, Long> { }
+import java.util.List;
+
+public interface SpeakerJpaRepository extends JpaRepository<Speaker, Long> {
+    List<Speaker> findByFirstNameAndLastName(String first, String last);
+    List<Speaker> findByFirstNameOrLastName(String first, String last);
+}
--- conference-data-jpa/src/test/java/com/pluralsight/conferencedemo/models/SpeakerTest.java
+++ conference-data-jpa/src/test/java/com/pluralsight/conferencedemo/models/SpeakerTest.java
@@ -1,5 +1,6 @@
 package com.pluralsight.conferencedemo.models;
 
+import com.pluralsight.conferencedemo.repositories.SpeakerJpaRepository;
 import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,20 +10,23 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class SpeakerTest {
     @Autowired
-    private SpeakerRepository repository;
+    private SpeakerJpaRepository repository;
 
     @PersistenceContext
     private EntityManager entityManager;
 
     @Test
     public void testFind() throws Exception {
-        Speaker speaker = repository.find(1L);
+        Speaker speaker = repository.getOne(1L);
         assertNotNull(speaker);
     }
 
@@ -35,16 +39,27 @@ public class SpeakerTest {
         s.setLastName("Bunker");
         s.setTitle("Author");
         s.setSpeakerBio("Consulting and mentoring");
-        s = repository.create(s);
+        s = repository.saveAndFlush(s);
 
         // clear the persistence context, so we don't return the previously cached location object
         // this is a test only thing and normally doesn't need to be done in prod code
         entityManager.clear();
 
-        Speaker otherSpeaker = repository.find(s.getSpeakerId());
+        Speaker otherSpeaker = repository.getOne(s.getSpeakerId());
         assertEquals("Dan", otherSpeaker.getFirstName());
 
-        repository.delete(otherSpeaker.getSpeakerId());
+        repository.deleteById(otherSpeaker.getSpeakerId());
     }
 
+    @Test
+    public void testJpaAnd() throws Exception {
+        List<Speaker> speakers = repository.findByFirstNameAndLastName("Justin", "Clark");
+        assertTrue(speakers.size() > 0);
+    }
+
+    @Test
+    public void testJpaOr() throws Exception {
+        List<Speaker> speakers = repository.findByFirstNameOrLastName("Justin", "Clark");
+        assertTrue(speakers.size() > 0);
+    }
 }
