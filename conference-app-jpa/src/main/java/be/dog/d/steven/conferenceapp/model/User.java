86006bdcdd9ca0e1dc4f9412f56bf6b9ae449ef7
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-10-18 20:50:45 +0200

    :sparkles: Entity

--- conference-app-jpa/src/main/java/be/dog/d/steven/conferenceapp/model/User.java
+++ conference-app-jpa/src/main/java/be/dog/d/steven/conferenceapp/model/User.java
@@ -1,9 +1,25 @@
 package be.dog.d.steven.conferenceapp.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
     private String firstname;
     private String lastname;
     private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getFirstname() {
         return firstname;
