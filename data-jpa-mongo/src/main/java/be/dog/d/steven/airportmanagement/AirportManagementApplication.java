12c56c7ea77fd564d7f0295e7a68997e9a04fcf0
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-10-22 00:43:22 +0200

    :rocket: Initial commit

---
+++ data-jpa-mongo/src/main/java/be/dog/d/steven/airportmanagement/AirportManagementApplication.java
@@ -0,0 +1,13 @@
+package be.dog.d.steven.airportmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirportManagementApplication.class, args);
+    }
+
+}
