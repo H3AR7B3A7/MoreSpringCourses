34957a72befcba56cdea629d0d48625727b64c95
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-10-23 22:06:52 +0200

    :sparkles: DatabaseSeeder, using MongoRepository

--- data-jpa-mongo/src/main/java/be/dog/d/steven/airportmanagement/DatabaseSeederRunner2.java
+++ data-jpa-mongo/src/main/java/be/dog/d/steven/airportmanagement/DatabaseSeederRunner2.java
@@ -2,12 +2,13 @@ package be.dog.d.steven.airportmanagement;
 
 import be.dog.d.steven.airportmanagement.domain.Aircraft;
 import be.dog.d.steven.airportmanagement.domain.FlightInformation;
+import be.dog.d.steven.airportmanagement.domain.FlightPrinter;
 import be.dog.d.steven.airportmanagement.domain.FlightType;
+import be.dog.d.steven.airportmanagement.repository.FlightInformationRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.core.annotation.Order;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -15,21 +16,21 @@ import java.util.Arrays;
 import java.util.List;
 
 @Component
-@Order(1) // This runs first, and will be overridden by DatabaseSeederRunner2.
+@Order(3) // This overrides the previous DatabaseSeederRunner. Change order for MongoTemplate implementation.
 @ConditionalOnProperty(
         prefix = "airport",
         value = "seeding.enabled",
         havingValue = "true",
         matchIfMissing = true)
-public class DatabaseSeederRunner implements CommandLineRunner {
-    private final MongoTemplate mongoTemplate;
+public class DatabaseSeederRunner2 implements CommandLineRunner {
+    private final FlightInformationRepository repository;
 
-    public DatabaseSeederRunner(MongoTemplate mongoTemplate) {
-        this.mongoTemplate = mongoTemplate;
+    public DatabaseSeederRunner2(FlightInformationRepository repository) {
+        this.repository = repository;
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         empty();
         seed();
     }
@@ -37,6 +38,7 @@ public class DatabaseSeederRunner implements CommandLineRunner {
     private void seed() {
         // Data
         FlightInformation flightOne = new FlightInformation();
+        flightOne.setId("b8b50563-ca90-4afc-9d43-b674892a525c");
         flightOne.setDelayed(false);
         flightOne.setDepartureCity("Rome");
         flightOne.setDestinationCity("Paris");
@@ -47,6 +49,7 @@ public class DatabaseSeederRunner implements CommandLineRunner {
         flightOne.setDescription("Flight from Rome to Paris");
 
         FlightInformation flightTwo = new FlightInformation();
+        flightTwo.setId("c0725fbb-eebb-4aae-8b41-3887793d0c50");
         flightTwo.setDelayed(false);
         flightTwo.setDepartureCity("New York");
         flightTwo.setDestinationCity("Copenhagen");
@@ -57,6 +60,7 @@ public class DatabaseSeederRunner implements CommandLineRunner {
         flightTwo.setDescription("Flight from NY to Copenhagen via Rome");
 
         FlightInformation flightThree = new FlightInformation();
+        flightThree.setId("bd8bc9ab-3bdc-4f57-8871-0c43501dc5c6");
         flightThree.setDelayed(true);
         flightThree.setDepartureCity("Bruxelles");
         flightThree.setDestinationCity("Bucharest");
@@ -66,25 +70,28 @@ public class DatabaseSeederRunner implements CommandLineRunner {
         flightThree.setAircraft(new Aircraft("A320", 170));
 
         FlightInformation flightFour = new FlightInformation();
+        flightFour.setId("73f478e5-cb3d-415d-ae20-e37f0df3231d");
         flightFour.setDelayed(true);
         flightFour.setDepartureCity("Madrid");
         flightFour.setDestinationCity("Barcelona");
         flightFour.setDepartureDate(LocalDate.of(2019, 6, 12));
-        flightFour.setType(FlightType.International);
+        flightFour.setType(FlightType.National);
         flightFour.setDurationMin(120);
         flightFour.setAircraft(new Aircraft("A319", 150));
 
         FlightInformation flightFive = new FlightInformation();
+        flightFive.setId("51cd0e4f-1e42-4e48-a961-64f889eee6b5");
         flightFive.setDelayed(false);
         flightFive.setDepartureCity("Las Vegas");
         flightFive.setDestinationCity("Washington");
         flightFive.setDepartureDate(LocalDate.of(2019, 6, 10));
-        flightFive.setType(FlightType.International);
+        flightFive.setType(FlightType.National);
         flightFive.setDurationMin(400);
         flightFive.setAircraft(new Aircraft("A319", 150));
         flightTwo.setDescription("Flight from LA to Washington via Paris");
 
         FlightInformation flightSix = new FlightInformation();
+        flightSix.setId("4d23fd8b-47a7-45f8-958c-94d0e21488b2");
         flightSix.setDelayed(false);
         flightSix.setDepartureCity("Bucharest");
         flightSix.setDestinationCity("Rome");
@@ -102,11 +109,23 @@ public class DatabaseSeederRunner implements CommandLineRunner {
                 flightFive,
                 flightSix
         );
-        this.mongoTemplate.insertAll(flights);
+
+        this.repository.insert(flights);
+
+        // Count
+        long count = this.repository.count();
+        System.out.println("Total flights in database : " + count);
+
+        // Print
+        List<FlightInformation> flightsInDb = this.repository
+                .findAll(Sort.by("departureCity").ascending());
+        FlightPrinter.print(flightsInDb);
+
+        System.out.println("--- Seeder finished ---\n");
     }
 
 
     private void empty() {
-        this.mongoTemplate.remove(new Query(), FlightInformation.class);
+        this.repository.deleteAll();
     }
 }
