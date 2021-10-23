9801a341cf501f9587d4f138bb835fa60d506dcd
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-10-23 12:14:24 +0200

    :sparkles: Converters

--- data-jpa-mongo/src/main/java/be/dog/d/steven/airportmanagement/AirportManagementApplication.java
+++ data-jpa-mongo/src/main/java/be/dog/d/steven/airportmanagement/AirportManagementApplication.java
@@ -1,7 +1,15 @@
 package be.dog.d.steven.airportmanagement;
 
+import be.dog.d.steven.airportmanagement.converter.AircraftDbReadConverter;
+import be.dog.d.steven.airportmanagement.converter.AircraftDbWriteConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class AirportManagementApplication {
@@ -9,5 +17,12 @@ public class AirportManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(AirportManagementApplication.class, args);
     }
-
+    
+    @Bean
+    public MongoCustomConversions customConversions() {
+        List<Converter<?, ?>> converters = new ArrayList<>();
+        converters.add(new AircraftDbReadConverter());
+        converters.add(new AircraftDbWriteConverter());
+        return new MongoCustomConversions(converters);
+    }
 }
