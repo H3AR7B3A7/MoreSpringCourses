ff061f2eaea0d5d976956c6a195136c86efbd59b
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-10-29 00:27:24 +0200

    :sparkles: User configuration

--- crypto-portfolio/module_4/mod4_crypto_portfolio_base/src/main/java/com/pluralsight/security/configuration/SecurityConfiguration.java
+++ crypto-portfolio/module_4/mod4_crypto_portfolio_base/src/main/java/com/pluralsight/security/configuration/SecurityConfiguration.java
@@ -1,9 +1,12 @@
 package com.pluralsight.security.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -24,4 +27,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().mvcMatchers("/css/**","/webjars/**");
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth
+                .inMemoryAuthentication()
+//                    .withUser("user").password("{noop}pass").roles("ADMIN");
+                    .withUser("user").password(encoder.encode("pass")).roles("ADMIN");
+    }
+    
 }
