bc8d2cf3854e6ac47010625b1c605293d0a9c00e
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-10-28 23:34:17 +0200

    :sparkles: Custom FilterChain

---
+++ crypto-portfolio/module_4/mod4_crypto_portfolio_base/src/main/java/com/pluralsight/security/configuration/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.pluralsight.security.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .mvcMatchers("/login").permitAll()
+                .mvcMatchers("/support/admin").hasRole("ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and()
+//                .httpBasic();
+                .formLogin().loginPage("/login");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().mvcMatchers("/css/**","/webjars/**");
+    }
+}
