2147b0d6560decfc4738be2dd996d72832b4d962
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-11-01 18:20:16 +0100

    :rocket: Initial commit

---
+++ conference-security/src/test/java/be/dog/d/steven/conferencesecurity/ConferenceSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package be.dog.d.steven.conferencesecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
