047e63a1fb9144d9aba9c5e96ddaad4fc40042a2
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-11-15 23:58:51 +0100

    :sparkles: Securing Methods

--- conference-security/src/main/java/com/pluralsight/conference/ConferenceSecurityConfig.java
+++ conference-security/src/main/java/com/pluralsight/conference/ConferenceSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,6 +20,10 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true)
 public class ConferenceSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -42,7 +47,7 @@ public class ConferenceSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/assets/css/**", "/assets/js/**", "/images/**", "/webjars/**").permitAll()
                 .antMatchers("/index*").permitAll()
                 .anyRequest().authenticated()
-                
+
                 .and()
                 .formLogin()
                 .loginPage("/login")
