b62bbe8efcbd4ead094226ddb537e3c34eb7ad06
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2022-09-21 20:29:55 +0200

    :sparkles: API Client

---
+++ authorization-servers/spring-authorization-server/resource-server/src/main/java/be/dog/d/steven/OAuth2ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package be.dog.d.steven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2ResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2ResourceServerApplication.class, args);
+    }
+
+}
